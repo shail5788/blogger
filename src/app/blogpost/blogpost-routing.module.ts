6bbae3f89e5090c21ad763e18b352170321c16cc
shailendra <shail5788@gmail.com>
2018-11-29 17:39:30 +0530

    add blog detail component

--- src/app/blogpost/blogpost-routing.module.ts
+++ src/app/blogpost/blogpost-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BlogpostListComponent } from "./blogpost-list/blogpost-list.component";
+import { BlogpostDetailComponent } from "./blogpost-detail/blogpost-detail.component";
 
 const routes: Routes = [
-  { path: "blog", component: BlogpostListComponent }
+  { path: "blog", component: BlogpostListComponent },
+  { path: "blog/:id", component: BlogpostDetailComponent }
 ];
 
 @NgModule({
